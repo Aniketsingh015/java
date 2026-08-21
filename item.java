c434934ffc587dbbbfed1d0f20c85f16b8f25289
Aniket Singh <asaniket.singh422@gmail.com>
2026-08-21 12:15:55 +0530

    item added

--- item.java
+++ item.java
@@ -1,3 +1,39 @@
 public class item {
-    
+    private int itemNo;
+    private String title;
+    private double rate;
+
+
+    public int getitemNo(){
+        return this.itemNo;
+    }
+
+    public void setitemNo(int itemNo){
+        this.itemNo=itemNo;
+    }
+
+    public String title(){
+        return this.title;
+    }
+
+    public void setTitle(String title){
+        this.title=title;
+    }
+
+    public double getrate(){
+        return this.rate;
+    }
+
+    public void setrate(double rate){
+        this.rate=rate;
+    }
+
+
+    // constructor
+    public item(int itemNo,String title,double rate){
+        this.itemNo=itemNo;
+        this.title=title;
+        this.rate=rate;
+
+    }
 }
