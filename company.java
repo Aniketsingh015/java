156859e033b448c4f4005652814c14381a331e48
Aniket Singh <asaniket.singh422@gmail.com>
2026-08-21 12:09:35 +0530

    first commit

---
+++ company.java
@@ -0,0 +1,15 @@
+class company{
+    private String name;
+
+    public void setName(String Name){//setters
+        this.name=name;
+    }
+
+    public String getName(){//getters
+        return this.name;
+    }
+
+    public company(String Name){
+        this.name=name;
+    }
+}
