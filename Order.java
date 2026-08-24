ce94ed8edbdc2315df60ec3425982a55d8f8d9c9
Aniket Singh <asaniket.singh422@gmail.com>
2026-08-24 10:59:18 +0530

    order update with order Items

--- Order.java
+++ Order.java
@@ -1,6 +1,11 @@
+import java.util.*;
+// here we have added customer has a relation of one ,and order item has a relationship of many
+
 public class Order {
     private int orderId;
     private Customer customer;
+
+    private List<OrderItem>orderItems;
 
     public int getOrderId(){
         return this.orderId=orderId;
@@ -15,5 +20,17 @@ public class Order {
     }
     public void setCustomer(Customer customer){
         this.customer=customer;
+    }
+
+    public List<OrderItem> getorderItems(){
+        return this.orderItems;
+    }
+
+    public void addOrderItem(OrderItem orderItem){
+        this.orderItems.add(orderItem);
+    }
+
+    public Order(){
+        this.orderItems=new ArrayList<OrderItem>();
     }
 }
