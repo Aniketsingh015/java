282fb82773269e51363bd885c4d45b0e02a62173
Aniket Singh <asaniket.singh422@gmail.com>
2026-08-24 09:40:10 +0530

    order without relation

---
+++ Order.java
@@ -0,0 +1,11 @@
+public class Order {
+    private int orderId;
+
+    public int getOrderId(){
+        return this.orderId=orderId;
+    }
+
+    public void setorderId(int orderId){
+        this.orderId=orderId;
+    }
+}
