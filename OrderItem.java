52a402a105982a05861130a2533cc040095f7343
Aniket Singh <asaniket.singh422@gmail.com>
2026-08-24 10:58:33 +0530

    order item for each customer

---
+++ OrderItem.java
@@ -0,0 +1,20 @@
+public class OrderItem {
+    private Item item;
+    private int quantity;
+
+
+    public Item getItem(){
+        return this.item;
+    }
+    public void setItem(Item item){
+        this.item=item;
+    }
+
+
+    public int  getQuantity(){
+        return this.quantity;
+    }
+    public void setQuantity(int quantity){
+        this.quantity=quantity;
+    }
+}
