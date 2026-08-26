0f1b867473d123dd0f89277a6cc190d1035eeff2
Aniket Singh <asaniket.singh422@gmail.com>
2026-08-26 17:17:10 +0530

    registered customer

---
+++ RegisteredCustomer.java
@@ -0,0 +1,19 @@
+
+public class RegisteredCustomer extends Customer{
+
+public RegisteredCustomer(String Name) {
+        super(Name);
+        //TODO Auto-generated constructor stub
+    }
+private double discount;
+
+public double getDiscount(){
+        return this.discount;
+    }
+ public void setDiscount(double discount){
+        this.discount=discount;
+    }
+
+
+}
+
