26e70bbc352dfd4c1b7c8b97e5c4975f702cf531
Aniket Singh <asaniket.singh422@gmail.com>
2026-09-03 15:14:32 +0530

    refactor: clean up commented code and clarify membership relationship

--- RegisteredCustomer.java
+++ RegisteredCustomer.java
@@ -1,55 +1,67 @@
 
 public class RegisteredCustomer extends Customer{
 
+    // registered customer has a relationship-one registered cust has 1 membership.unidirectional
+    private Membership membership;
+
+
 public RegisteredCustomer(String Name) {
         super(Name);
-        //TODO Auto-generated constructor stub
-    }
-private double discount;
-private double fees;
-private String membershipType;
-
-public double getDiscount(){
-        return this.discount;
-    }
- public void setDiscount(double discount){
-        this.discount=discount;
     }
 
-    public double getFees(){
-        return this.fees;
-    }
- public void setFees(double fees){
-        this.fees=fees;
-    }
+//     private  Membership membership;
+// private double discount;
+// private double fees;
+// private String membershipType;
 
-
-    public String getmembershipType(){
-        return this.membershipType;
+public Membership getMembership(){
+        return this.membership;
     }
- public void setmembershipType(String membershipType){
-        this.membershipType=membershipType;
+ public void setmembership(Membership membership){
+        this.membership=membership;
     }
 
+// public double getDiscount(){
+//         return this.discount;
+//     }
+//  public void setDiscount(double discount){
+//         this.discount=discount;
+//     }
 
-    // functional method
-    @Override
-    public double getTotalOrderValue(){
-        // here logic with discount
-        double totalOrderValue =0.0;
+//     public double getFees(){
+//         return this.fees;
+//     }
+//  public void setFees(double fees){
+//         this.fees=fees;
+//     }
+
+
+//     public String getmembershipType(){
+//         return this.membershipType;
+//     }
+//  public void setmembershipType(String membershipType){
+//         this.membershipType=membershipType;
+//     }
+
+
+    // // functional method
+    // @Override
+    // public double getTotalOrderValue(){
+    //     // here logic with discount
+    //     double totalOrderValue =0.0;
         
-            for(Order order:this.getOrders()){//then go to the order of that customer
+    //         for(Order order:this.getOrders()){//then go to the order of that customer
                 
                     
-                        double discount=this.getDiscount();//here we are getting the discount of the registered customer
-                        totalOrderValue+=order.getTotalOrderValue()*((1-discount)/100);
-                    }
+    //                     double discount=this.getDiscount();//here we are getting the discount of the registered customer
+    //                     totalOrderValue+=order.getTotalOrderValue()*((1-discount)/100);
+    //                 }
             
             
         
 
-        return totalOrderValue;
-    }
+    //     return totalOrderValue;
+    // }
 }
         
 
